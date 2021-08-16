f2842b244262b898c9d6e1e65ce2397d6cc43c23
ffewster <88655049+ffewster@users.noreply.github.com>
2021-08-16 12:55:00 +0100

    Chapter 50 - Evicting cart items from the cache (#27)

--- sick-fits/frontend/components/Cart/RemoveFromCart.jsx
+++ sick-fits/frontend/components/Cart/RemoveFromCart.jsx
@@ -23,11 +23,16 @@ const REMOVE_FROM_CART_MUTATION = gql`
   }
 `;
 
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+}
+
 const RemoveFromCart = ({ id }) => {
   const [removeFromCart, { loading, error, data }] = useMutation(
     REMOVE_FROM_CART_MUTATION,
     {
       variables: { id },
+      update,
     }
   );
 
