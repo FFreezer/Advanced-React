e2114e51a7389a6742aa068c4d75449b2fda690e
ffewster <88655049+ffewster@users.noreply.github.com>
2021-08-18 14:21:22 +0100

    Chapter 64 - Permissions access functions (#40)

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -2,9 +2,22 @@
 // a yes or a no value depending on the users session
 
 import { ListAccessArgs, Session } from './types';
-
+import { Permission, permissionsList } from './schemas/fields';
 export const isSignedIn = ({ session } : ListAccessArgs) => !!session;
 
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission, 
+    ({ session } : ListAccessArgs) => !!session.data.role[permission]
+  ])
+);
 
-
- 
+// Permissions check if someone meets a criteria yes or no
+export const permissions = {
+  ...generatedPermissions,
+  can: (permission: Permission, {session}: ListAccessArgs) => {
+    return () => {
+      return session.data.role[permission];
+    }
+  }
+}
